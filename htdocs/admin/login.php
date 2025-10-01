725518ca8a7e3ffb2e9b152362429548f75d6c60
Vichingo455 <vichingo455@gmail.com>
2025-10-01 17:09:19 +0200

    move error message

--- htdocs/admin/login.php
+++ htdocs/admin/login.php
@@ -39,12 +39,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <!-- Container login -->
   <div class="login-container">
     <h1>Login Admin</h1>
-    <?php if(isset($error)) echo "<div class='error'>$error</div>"; ?>
     <form method="post">
       <input type="text" name="username" placeholder="Username" required><br>
       <input type="password" name="password" placeholder="Password" required><br>
       <button type="submit">Login</button>
     </form>
+    <?php if(isset($error)) echo "<div class='error'>$error</div>"; ?>
   </div>
 <p style="text-align: center;">Copyright (C) 2025 EmmeV. - Released under <a href="https://git.vichingo455.freeddns.org/emmev-code/orario/src/branch/stable/LICENSE.txt" target="_blank">GNU AGPL 3.0 License</a>.</p>
 </body>
