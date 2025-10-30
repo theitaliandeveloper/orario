721cfc386dcf7631975479465f31a577619b98b3
Vichingo455 <vichingo455@gmail.com>
2025-10-30 08:26:16 +0100

    I don't care about Google SSO

--- docker/php/config.php
+++ docker/php/config.php
@@ -44,19 +44,4 @@ if (AUTH_TYPE === 'keycloak') {
         define('KEYCLOAK_CLIENT_SECRET',''); // Client Secret per Keycloak (ad esempio abcdefghijklm)
     }
 }
-// Impostazioni autenticazione con Google (richieste solo se AUTH_TYPE sta impostato su google)
-if (AUTH_TYPE === 'google') {
-    if (!defined('GOOGLE_CLIENT_ID')) {
-        define('GOOGLE_CLIENT_ID',''); // Client ID fornito da Google
-    }
-    if (!defined('GOOGLE_CLIENT_SECRET')) {
-        define('GOOGLE_CLIENT_SECRET',''); // Client Secret fornito da Google
-    }
-    if (!defined('GOOGLE_ONLY_ALLOWED_DOMAINS')) {
-        define('GOOGLE_ONLY_ALLOWED_DOMAINS', false); // Attivare (impostare su true) per impostare restrizioni sui domini e-mail consentiti
-    }
-    if (!defined('GOOGLE_ALLOWED_DOMAINS')) {
-        define('GOOGLE_ALLOWED_DOMAINS', ['']); // Domini E-Mail consentiti. Serve abilitare l'opzione GOOGLE_ONLY_ALLOWED_DOMAINS
-    }
-}
 ?>
--- htdocs/config/config.php
+++ htdocs/config/config.php
@@ -24,7 +24,7 @@ if (!defined('DEV_MODE')) {
 }
 // Impostazioni autenticazione dashboard amministrativa
 if (!defined('AUTH_TYPE')) {
-    define('AUTH_TYPE','local'); // Può essere local (integrata), keycloak, google
+    define('AUTH_TYPE','local'); // Può essere local (integrata), keycloak
 }
 if (!defined('APP_DOMAIN')) {
     define('APP_DOMAIN',''); // Dominio del sito (ad esempio orario.yourdomain.com), richiesto per autenticazioni non local
@@ -42,21 +42,6 @@ if (AUTH_TYPE === 'keycloak') {
     }
     if (!defined('KEYCLOAK_CLIENT_SECRET')) {
         define('KEYCLOAK_CLIENT_SECRET',''); // Client Secret per Keycloak (ad esempio abcdefghijklm)
-    }
-}
-// Impostazioni autenticazione con Google (richieste solo se AUTH_TYPE sta impostato su google)
-if (AUTH_TYPE === 'google') {
-    if (!defined('GOOGLE_CLIENT_ID')) {
-        define('GOOGLE_CLIENT_ID',''); // Client ID fornito da Google
-    }
-    if (!defined('GOOGLE_CLIENT_SECRET')) {
-        define('GOOGLE_CLIENT_SECRET',''); // Client Secret fornito da Google
-    }
-    if (!defined('GOOGLE_ONLY_ALLOWED_DOMAINS')) {
-        define('GOOGLE_ONLY_ALLOWED_DOMAINS', false); // Attivare (impostare su true) per impostare restrizioni sui domini e-mail consentiti
-    }
-    if (!defined('GOOGLE_ALLOWED_DOMAINS')) {
-        define('GOOGLE_ALLOWED_DOMAINS', ['']); // Domini E-Mail consentiti. Serve abilitare l'opzione GOOGLE_ONLY_ALLOWED_DOMAINS
     }
 }
 ?>
