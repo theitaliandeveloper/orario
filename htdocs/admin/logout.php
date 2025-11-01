1453a8028e0b10a872f484854c3a1a7221d3fa5d
Vichingo455 <vichingo455@gmail.com>
2025-11-01 11:00:07 +0100

    empty repo first

--- htdocs/admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header('Location: https://<KEYCLOAK_URL>/realms/<REALM>/protocol/openid-connect/logout?post_logout_redirect_uri=https://<APP_DOMAIN>&client_id=<CLIENT_ID>');
-exit;
