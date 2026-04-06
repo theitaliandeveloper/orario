88bcb61c7ce5affd82232ca3fe29f1be6eb6e077
Curry141 <135717077+Daniongithub@users.noreply.github.com>
2026-04-06 15:31:44 +0200

    Creo API per interfacciarmi con i laboratori.

---
+++ htdocs/api/getLabs.php
@@ -0,0 +1,9 @@
+<?php
+include("../lib/db.php");
+$res = $conn->query("SELECT DISTINCT room FROM subjects WHERE room IS NOT NULL AND room != '' ORDER BY room");
+$rooms = [];
+while ($row = $res->fetch_assoc()) {
+    $rooms[] = $row['room'];
+}
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($rooms, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
